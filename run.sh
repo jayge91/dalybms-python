4306dfe696f2a1b1e51f70c082dec5b2230b1c7d
jayge91 <39917492+jayge91@users.noreply.github.com>
2024-03-08 21:07:09 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,19 +3,19 @@
 
 # Check if bms-mqtt.py is in the /script folder and make a new one if not:
 if ! test -f /dalybms/bms-mqtt.py; then
-  echo "bms-mqtt.py is not in the /script folder. Creating a new copy..."
+  echo "bms-mqtt.py is not in the /dalybms folder. Creating a new copy..."
   cp /usr/src/app/bms-mqtt.py /dalybms/bms-mqtt.py
   echo "Done."
 fi
 
 # Check if default.env is in the /script folder and make a new one if not:
 if ! test -f /dalybms/default.env; then
-  echo "default.env is not in the /script folder. Creating a new copy..."
+  echo "default.env is not in the /dalybms folder. Creating a new copy..."
   cp /usr/src/app/bms-mqtt.py /dalybms/bms-mqtt.py
   echo "Done."
 fi
 
 echo "Starting BMS Monitor"
-python /script/bms-mqtt.py
+python /dalybms/bms-mqtt.py
 
 echo "Script stopped!"
