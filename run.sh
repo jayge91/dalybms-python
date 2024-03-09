681faf03b65e93d015b9757b14f4e3cfa9cbe0fd
jayge91 <39917492+jayge91@users.noreply.github.com>
2024-03-08 21:57:01 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,11 +9,11 @@ echo "Done."
 #fi
 
 # Check if default.env is in the /script folder and make a new one if not:
-if ! test -f /dalybms/default.env; then
-  echo "default.env is not in the /dalybms folder. Creating a new copy..."
-  cp /usr/src/app/default.env /dalybms/default.env
-  echo "Done."
-fi
+#if ! test -f /dalybms/default.env; then
+echo "default.env is not in the /dalybms folder. Creating a new copy..."
+cp /usr/src/app/default.env /dalybms/default.env
+echo "Done."
+#fi
 
 echo "Starting BMS Monitor"
 python /dalybms/bms-mqtt.py
