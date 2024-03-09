66786bcaa2bdaa8ba2dc12b521245786d646053c
jayge91 <39917492+jayge91@users.noreply.github.com>
2024-03-08 20:37:27 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+
 # Check if bms-mqtt.py is in the /script folder and make a new one if not:
-if test -f /script/bms-mqtt.py; then
-  echo "bms-mqtt.py exists! Starting BMS Monitor...."
-else
+if ! test -f /script/bms-mqtt.py; then
   echo "mbs-mqtt.py is not in the /script folder. Creating a new copy..."
   cp /usr/src/app/bms-mqtt.py /script/bms-mqtt.py
-  echo "Done. Starting BMS Monitor...."
+  echo "Done."
 fi
 
-python3 /script/bms-mqtt.py
+# Check if default.env is in the /script folder and make a new one if not:
+if ! test -f /script/default.env; then
+  echo "mbs-mqtt.py is not in the /script folder. Creating a new copy..."
+  cp /usr/src/app/bms-mqtt.py /script/bms-mqtt.py
+  echo "Done."
+fi
+
+echo "Starting BMS Monitor"
+python /script/bms-mqtt.py
+
+echo "Script stopped!"
