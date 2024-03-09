05e71d628ecb692da3b3a745efc52f9e4c77d82d
jayge91 <39917492+jayge91@users.noreply.github.com>
2024-03-08 20:25:41 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-CONFIG_PATH=/data/options.json
 
-MQTT_SERVER="$(jq --raw-output '.mqtt_server' $CONFIG_PATH)" \
-MQTT_USER="$(jq --raw-output '.mqtt_user' $CONFIG_PATH)" \
-MQTT_PASS="$(jq --raw-output '.mqtt_pass' $CONFIG_PATH)" \
-MQTT_CLIENT_ID="$(jq --raw-output '.mqtt_client_id' $CONFIG_PATH)" \
-MQTT_DISCOVERY_PREFIX="$(jq --raw-output '.mqtt_discovery_prefix' $CONFIG_PATH)" \
-DEVICE="$(jq --raw-output '.device' $CONFIG_PATH)" \
-DEVICE_ID="$(jq --raw-output '.device_id' $CONFIG_PATH)" \
-CELL_COUNT="$(jq --raw-output '.cells_in_series' $CONFIG_PATH)" \
-python3 /monitor.py
+# Check if bms-mqtt.py is in the /script folder and make a new one if not:
+if test -f /script/bms-mqtt.py; then
+  echo "bms-mqtt.py exists! Starting BMS Monitor...."
+else
+  echo "mbs-mqtt.py is not in the /script folder. Creating a new copy..."
+  cp /usr/src/app/bms-mqtt.py /script/bms-mqtt.py
+  echo "Done. Starting BMS Monitor...."
+fi
+
+python3 /script/bms-mqtt.py
